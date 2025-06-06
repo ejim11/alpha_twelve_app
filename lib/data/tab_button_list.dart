e44646780bccdb17ed514c5c5b56b0336366753b
ejim11 <favourejim56@gmail.com>
2025-06-06 13:22:12 +0100

    ✅ created the list of tab button item

---
+++ lib/data/tab_button_list.dart
@@ -0,0 +1,8 @@
+import 'package:alpha_twelve_app/models/tab_button_item.dart';
+
+List<TabButtonItem> tabButtonList = [
+  TabButtonItem(tabIcon: "assets/home-icon.png", tabLabel: "Home"),
+  TabButtonItem(tabIcon: "assets/cart-icon.png", tabLabel: "Cart"),
+  TabButtonItem(tabIcon: "assets/heart-icon.png", tabLabel: "Favourites"),
+  TabButtonItem(tabIcon: "assets/profile-icon.png", tabLabel: "Profile"),
+];
