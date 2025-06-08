21af4e5b44c87908bf9c8f2d93dc2258085e2ed0
ejim11 <favourejim56@gmail.com>
2025-06-08 13:01:18 +0100

    ✅ updated  tabs import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alpha_twelve_app/screens/tabs.dart';
+import 'package:alpha_twelve_app/screens/tabs/tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
