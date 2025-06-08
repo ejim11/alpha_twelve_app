b636fa56065d426104e178cf425507a7f8d84452
ejim11 <favourejim56@gmail.com>
2025-06-08 13:01:36 +0100

    🗃️ shop item list data

---
+++ lib/data/shop_item_list.dart
@@ -0,0 +1,41 @@
+import 'package:alpha_twelve_app/models/shop_item.dart';
+
+List<ShopItem> shopItemList = [
+  ShopItem(
+    image: 'assets/shop-items/apple-iPhone-16-128GB-Teal.png',
+    name: 'Apple iPhone 16 128GB|Teal',
+    price: 700,
+    description: [
+      'This pre-owned product is not Apple certified, but has been professionally inspected, tested and cleaned by Amazon-qualified suppliers.',
+      'There will be no visible cosmetic imperfections when held at an arm’s length. There will be no visible cosmetic imperfections when held at an arm’s length.',
+    ],
+  ),
+  ShopItem(
+    image: 'assets/shop-items/M4-Macbook-Air-13-256GB-Sky-blue.png',
+    name: 'M4 Macbook Air 13” 256GB|Sky blue',
+    price: 1000,
+    description: [
+      'This pre-owned product is not Apple certified, but has been professionally inspected, tested and cleaned by Amazon-qualified suppliers.',
+      'There will be no visible cosmetic imperfections when held at an arm’s length. There will be no visible cosmetic imperfections when held at an arm’s length.',
+    ],
+  ),
+  ShopItem(
+    image: 'assets/shop-items/Google-Pixel-9A-128GB-Iris.png',
+    name: 'Google Pixel 9A 128GB|Iris',
+    price: 499,
+    description: [
+      'This pre-owned product is not Apple certified, but has been professionally inspected, tested and cleaned by Amazon-qualified suppliers.',
+      'There will be no visible cosmetic imperfections when held at an arm’s length. There will be no visible cosmetic imperfections when held at an arm’s length.',
+    ],
+  ),
+  ShopItem(
+    image:
+        'assets/shop-items/Apple-Airpods-4-Active-Noise-Cancellation-Teal.png',
+    name: 'Apple Airpods 4 Active Noise Cancellation|Teal',
+    price: 129,
+    description: [
+      'This pre-owned product is not Apple certified, but has been professionally inspected, tested and cleaned by Amazon-qualified suppliers.',
+      'There will be no visible cosmetic imperfections when held at an arm’s length. There will be no visible cosmetic imperfections when held at an arm’s length.',
+    ],
+  ),
+];
