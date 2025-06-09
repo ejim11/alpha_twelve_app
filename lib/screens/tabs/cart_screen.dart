a05cee34a766b2fbc1a12bfee5d3d13275ea8ccc
ejim11 <favourejim56@gmail.com>
2025-06-09 01:00:03 +0100

    ✅ cart screen

--- lib/screens/tabs/cart_screen.dart
+++ lib/screens/tabs/cart_screen.dart
@@ -1,17 +1,37 @@
+import 'package:alpha_twelve_app/providers/cart_provider.dart';
+import 'package:alpha_twelve_app/screens/tabs/tabs.dart';
+import 'package:alpha_twelve_app/widgets/cart/cart_item_card_list.dart';
+import 'package:alpha_twelve_app/widgets/home/address_and_searchbox_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends ConsumerWidget {
   const CartScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cartItems = ref.watch(cartProvider);
+
+    double safeAreaTopPadding = MediaQuery.of(context).padding.top;
+
     return Container(
       color: Theme.of(context).colorScheme.surface,
-      child: Center(
-        child: Text(
-          'Cart',
-          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-            color: Theme.of(context).colorScheme.onSurface,
+      child: SingleChildScrollView(
+        child: Container(
+          color: Theme.of(context).colorScheme.surface,
+          padding: EdgeInsets.only(top: safeAreaTopPadding),
+          child: Column(
+            children: [
+              AddressAndSearchBoxContainer(
+                backBtnText: 'Your Cart',
+                backBtnFunction: () {
+                  Navigator.of(
+                    context,
+                  ).push(MaterialPageRoute(builder: (ctx) => TabsScreen()));
+                },
+              ),
+              CartItemCardList(items: cartItems),
+            ],
           ),
         ),
       ),
